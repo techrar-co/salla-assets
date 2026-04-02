f34b146684378b6dc4112e977642fadfad2472f9
Zia Ur Rehman Khan <zmajrohi323@gmail.com>
2026-04-02 16:11:03 +0500

    chore: release jsdelivr 0.2.0

--- dist/salla/current.js
+++ dist/salla/current.js
@@ -1,10 +1,10 @@
 (function (w, d) {
 	if (w.__techrarSallaReleaseLoaded) return;
 	w.__techrarSallaReleaseLoaded = true;
-	w.__techrarSallaVersion = "0.1.1";
+	w.__techrarSallaVersion = "0.2.0";
 
 	var s = d.createElement('script');
-	s.src = "https://cdn.jsdelivr.net/gh/techrar-co/salla-assets@0.1.1/dist/salla/releases/salla-storefront-snippet.0.1.1.min.js";
+	s.src = "https://cdn.jsdelivr.net/gh/techrar-co/salla-assets@0.2.0/dist/salla/releases/salla-storefront-snippet.0.2.0.min.js";
 	s.async = true;
 	s.crossOrigin = 'anonymous';
 	(d.head || d.documentElement).appendChild(s);
